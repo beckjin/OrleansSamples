d4a8db85a8dc47ae79488b4d217f255eab8a0702
beckjin <beck.jin@mingdao.com>
2017-07-21 18:50:58 +0800

    fileStroage

---
+++ OrleansFileStorage/Grains/FooObserver.cs
@@ -0,0 +1,17 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    class FooObserver : IFooGrainObserver
+    {
+        public void Foo(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ OrleansFileStorage/Interfaces/IFooGrainObserver.cs
@@ -0,0 +1,14 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IFooGrainObserver : IGrainObserver
+    {
+        void Foo(string message);
+    }
+}
