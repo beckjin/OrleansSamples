04f13a652f0a971c6d39867f49371ef3053aaa41
beckjin <beck.jin@mingdao.com>
2017-07-27 13:00:26 +0800

    OrleansStreams

---
+++ OrleansStreams/Client/Program.cs
@@ -0,0 +1,56 @@
+﻿using Interfaces;
+using Orleans;
+using Orleans.Runtime;
+using Orleans.Runtime.Configuration;
+using Orleans.Streams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            var config = ClientConfiguration.LoadFromFile("ClientConfiguration.xml");
+
+            while (true)
+            {
+                try
+                {
+                    GrainClient.Initialize(config);
+                    Console.WriteLine("Connected to silo");
+                    break;
+                }
+                catch (SiloUnavailableException)
+                {
+                    Console.WriteLine("Silo not available! Retrying in 3 seconds.");
+                    Thread.Sleep(3000);
+                }
+            }
+
+            DoWork();
+
+            Console.ReadLine();
+        }
+
+        static async void DoWork()
+        {
+            var streamProvider = GrainClient.GetStreamProvider("SMSProvider");
+            var stream = streamProvider.GetStream<int>(Guid.Empty, "RANDOMDATA");
+            await stream.SubscribeAsync((data, token) =>
+            {
+                Console.WriteLine($"Received data: {data}");
+                return Task.CompletedTask;
+            });
+
+            var subscriberGrain = GrainClient.GrainFactory.GetGrain<IRandomReceiver>(new Guid());
+            await subscriberGrain.Start();
+        }
+    }
+}
---
+++ OrleansStreams/Grains/ReceiverGrain.cs
@@ -0,0 +1,33 @@
+﻿using Interfaces;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    //[ImplicitStreamSubscription("RANDOMDATA")]
+    public class ReceiverGrain : Grain, IRandomReceiver
+    {
+        public override Task OnActivateAsync()
+        {
+            var guid = this.GetPrimaryKey();
+
+            var streamProvider = GetStreamProvider("SMSProvider");
+            var stream = streamProvider.GetStream<int>(guid, "RANDOMDATA");
+
+            RegisterTimer(s => {
+                return stream.OnNextAsync(new Random().Next());
+            }, null, TimeSpan.FromMilliseconds(1000), TimeSpan.FromMilliseconds(1000));
+
+            return base.OnActivateAsync();
+        }
+
+        public Task Start()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
