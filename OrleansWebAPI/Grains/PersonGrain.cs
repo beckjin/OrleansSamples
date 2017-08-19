936adf01997b7e25531e1ea1dfb8c2a6e90b3a37
beckjin <beck.jin@mingdao.com>
2017-08-19 18:44:30 +0800

    OrleansWebAPI-模拟实际开发项目结构

---
+++ OrleansWebAPI/Grains/PersonGrain.cs
@@ -0,0 +1,18 @@
+﻿using Orleans;
+using Orleans.Providers;
+using Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Grains
+{
+    public class PersonGrain : Grain, IPersonGrain
+    {
+        public async Task SayHelloAsync()
+        {
+            string primaryKey = this.GetPrimaryKeyString();
+            Console.WriteLine($"{primaryKey} said hello!");
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ OrleansWebAPI/WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class PersonController : ApiController
+    {
+        [HttpGet]
+        public string SayHello(string name)
+        {
+            return new Business.Persion().SayHello(name);
+        }
+    }
+}
