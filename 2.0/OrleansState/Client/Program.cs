75ecc544f4f792fe4b808d8e531ba43736dc0725
金彪 <1798410296@qq.com>
2018-10-20 23:31:21 +0800

    OrleansState 2.0

---
+++ 2.0/OrleansState/Client/Program.cs
@@ -0,0 +1,76 @@
+﻿using Interfaces;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        private static IClusterClient client = null;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            client = InitialiseClient().Result;
+            if (client != null)
+            {
+                var joe = client.GetGrain<IPersonGrain>("Joe");
+                joe.SayHelloAsync();
+                joe.SayHelloAsync();
+
+                var sam = client.GetGrain<IPersonGrain>("Sam");
+                sam.SayHelloAsync();
+                sam.SayHelloAsync();
+            }
+            else
+            {
+                Console.WriteLine("Client init failed.");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static async Task<IClusterClient> InitialiseClient()
+        {
+            int tryTimes = 10;
+            while (tryTimes > 0)
+            {
+                try
+                {
+                    client = new ClientBuilder()
+                                .UseLocalhostClustering()
+                                .Configure<ClusterOptions>(options =>
+                                {
+                                    options.ClusterId = "dev";
+                                    options.ServiceId = "OrleansTest";
+                                })
+                                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IPersonGrain).Assembly))
+                                .ConfigureLogging(log => log.SetMinimumLevel(LogLevel.Warning).AddConsole())
+                                .Build();
+
+                    await client.Connect();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+
+                    if (client != null && !client.IsInitialized)
+                    {
+                        client.Dispose();
+                        client = null;
+                    }
+                }
+                tryTimes--;
+            }
+
+            return client;
+        }
+    }
+}
---
+++ 2.0/OrleansState/Silo/Program.cs
@@ -0,0 +1,49 @@
+﻿using Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Silo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Silo";
+            StartSilo().Wait();
+        }
+
+        private static async Task StartSilo()
+        {
+            var builder = new SiloHostBuilder()
+                //.AddMemoryGrainStorage("OrleansStorage", options => options.NumStorageGrains = 10)
+                .AddAdoNetGrainStorage("OrleansStorage", options =>
+                {
+                    options.Invariant = "MySql.Data.MySqlClient";
+                    options.ConnectionString = "server=localhost;port=3306;database=orleans;user id=root;password=;SslMode=none;";
+                    options.UseJsonFormat = true;
+                })
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "OrleansTest";
+                })
+                .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(PersonGrain).Assembly).WithReferences())
+                .ConfigureLogging(log => log.SetMinimumLevel(LogLevel.Warning).AddConsole());
+
+            var host = builder.Build();
+            await host.StartAsync();
+
+            Console.WriteLine("Silo started successfully");
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+            await host.StopAsync();
+        }
+    }
+}
