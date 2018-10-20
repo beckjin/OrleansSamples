e2e45fce77dee90145f01a68d95826a3a7d4ccb7
金彪 <1798410296@qq.com>
2018-10-21 00:42:32 +0800

    OrleansCluster 2.0

---
+++ 2.0/OrleansCluster/Client/Program.cs
@@ -0,0 +1,83 @@
+﻿using Interfaces;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        private static IClusterClient client = null;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+            client = InitialiseClient().Result;
+            if (client != null)
+            {
+                var random = new Random();
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                    var grainId = random.Next().ToString();
+                    var grain = client.GetGrain<IPersonGrain>("Test------" + grainId);
+                    grain.SayHelloAsync();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Client init failed.");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static async Task<IClusterClient> InitialiseClient()
+        {
+            int tryTimes = 10;
+            while (tryTimes > 0)
+            {
+                try
+                {
+                    client = new ClientBuilder()
+                                 .UseAdoNetClustering(options =>
+                                 {
+                                     options.Invariant = "MySql.Data.MySqlClient";
+                                     options.ConnectionString = "server=localhost;port=3306;database=orleans;user id=root;password=;SslMode=none;";
+                                 })
+                                .Configure<ClusterOptions>(options =>
+                                {
+                                    options.ClusterId = "dev";
+                                    options.ServiceId = "OrleansTest";
+                                })
+                                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IPersonGrain).Assembly))
+                                .ConfigureLogging(log => log.SetMinimumLevel(LogLevel.Warning).AddConsole())
+                                .Build();
+
+                    await client.Connect();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+
+                    if (client != null && !client.IsInitialized)
+                    {
+                        client.Dispose();
+                        client = null;
+                    }
+                }
+                tryTimes--;
+            }
+
+            return client;
+        }
+    }
+}
---
+++ 2.0/OrleansCluster/Silo/Program.cs
@@ -0,0 +1,60 @@
+﻿using Grains;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace Silo
+{
+    class Program
+    {
+        private const string Invariant = "MySql.Data.MySqlClient";
+        private const string ConnectionString = "server=localhost;port=3306;database=orleans;user id=root;password=;SslMode=none;";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输入Silo序号:");
+            var index = Convert.ToInt32(Console.ReadLine());
+
+            Console.Title = "Silo" + index;
+            StartSilo(11111 + index, 30000 + index).Wait();
+        }
+
+        private static async Task StartSilo(int siloPort, int gatewayPort)
+        {
+            var builder = new SiloHostBuilder()
+                // Grain State
+                .AddAdoNetGrainStorage("OrleansStorage", options =>
+                {
+                    options.Invariant = Invariant;
+                    options.ConnectionString = ConnectionString;
+                    options.UseJsonFormat = true;
+                })
+                // Membership
+                .UseAdoNetClustering(options =>
+                 {
+                     options.Invariant = Invariant;
+                     options.ConnectionString = ConnectionString;
+                 })
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "OrleansTest";
+                })
+                .ConfigureEndpoints(siloPort: siloPort, gatewayPort: gatewayPort)
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(PersonGrain).Assembly).WithReferences())
+                .ConfigureLogging(log => log.SetMinimumLevel(LogLevel.Warning).AddConsole());
+
+            var host = builder.Build();
+            await host.StartAsync();
+
+            Console.WriteLine("Silo started successfully");
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+            Console.WriteLine("-------------------------------");
+            await host.StopAsync();
+        }
+    }
+}
