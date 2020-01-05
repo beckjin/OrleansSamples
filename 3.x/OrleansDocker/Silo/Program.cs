b6e5dc569496e64b67591d0310ecc706a491e04d
beckjin <1798410296@qq.com>
2020-01-05 23:12:59 +0800

    Docker Deploy test

--- 3.x/OrleansDocker/Silo/Program.cs
+++ 3.x/OrleansDocker/Silo/Program.cs
@@ -6,9 +6,7 @@ using Orleans.Configuration;
 using Orleans.Hosting;
 using System;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Net.NetworkInformation;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,13 +24,8 @@ namespace Silo
                 .AddJsonFile("appsettings.json", false, true);
             var configuration = builder.Build();
 
-            // 以太网 IPv4
-            var nodeIps = NetworkInterface.GetAllNetworkInterfaces()
-                .Where(iface => iface.NetworkInterfaceType == NetworkInterfaceType.Ethernet).SelectMany(iface => iface.GetIPProperties().UnicastAddresses)
-                .Where(addr => addr.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).Select(addr => addr.Address)
-                .ToList();
-
-            IPAddress address = nodeIps[0];
+            var siloPort = int.Parse(configuration.GetSection("SiloPort").Value);
+            var gatewayPort = int.Parse(configuration.GetSection("GatewayPort").Value);
 
             silo = new SiloHostBuilder()
                  .Configure<ClusterOptions>(options =>
@@ -46,7 +39,7 @@ namespace Silo
                      options.ConnectionString = configuration.GetSection("ConnectionString").Value;
 
                  })
-                 .ConfigureEndpoints(address, 11111, 30000)
+                 .ConfigureEndpoints(Dns.GetHostName(), siloPort, gatewayPort)
                  .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(HelloGrain).Assembly).WithReferences())
                  .ConfigureLogging(log => log.SetMinimumLevel(LogLevel.Warning).AddConsole())
                  .Build();
@@ -56,11 +49,13 @@ namespace Silo
             Console.CancelKeyPress += (sender, e) =>
             {
                 Task.Run(StopSilo);
+                siloStopped.WaitOne();
             };
 
             AppDomain.CurrentDomain.ProcessExit += (s, e) =>
             {
                 Task.Run(StopSilo);
+                siloStopped.WaitOne();
             };
 
             siloStopped.WaitOne();
@@ -75,11 +70,8 @@ namespace Silo
 
         private static async Task StopSilo()
         {
-            if (silo != null)
-            {
-                await silo.StopAsync();
-                Console.WriteLine("Silo stopped");
-            }
+            await silo.StopAsync();
+            Console.WriteLine("Silo stopped");
             siloStopped.Set();
         }
     }
